b5a3078da1379aeeb7877ca7f6960ab928c3a13b
ejydavis <ejesyang@gmail.com>
2019-02-12 22:42:38 -0500

    Fixed error message issues

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegerationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                          email: "user@invalid",
+                                          password:             "foo",
+                                          password_confirmation:      "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error error_explanation>'
+    assert_template 'div.<CSS class for field with errors>'
+  end
+end
